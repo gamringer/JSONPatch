cf455080a5bf3728d75e0325653cb8a466dfe384
gamringer <g.amringer+github@gmail.com>
2014-10-31 23:38:55 -0400

    Formatting

--- src/ReferencedValue.php
+++ src/ReferencedValue.php
@@ -4,20 +4,20 @@ namespace gamringer\JSONPointer;
 
 class ReferencedValue
 {
-	private $value;
+    private $value;
 
-	public function __construct(&$value)
-	{
-		$this->value = &$value;
-	}
+    public function __construct(&$value)
+    {
+        $this->value = &$value;
+    }
 
-	public function getValue()
-	{
-		return $this->value;
-	}
+    public function getValue()
+    {
+        return $this->value;
+    }
 
-	public function setValue($value)
-	{
-		$this->value = $value;
-	}
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
 }
