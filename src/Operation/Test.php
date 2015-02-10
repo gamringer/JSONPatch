60319167bf9a85aaddec770a242ab60145f71669
gamringer <g.amringer+github@gmail.com>
2015-02-09 19:34:53 -0500

    asserting string and number values

--- src/Operation/Test.php
+++ src/Operation/Test.php
@@ -4,6 +4,7 @@ namespace gamringer\JSONPatch\Operation;
 
 use gamringer\JSONPatch\Operation;
 use gamringer\JSONPointer\Pointer;
+use gamringer\JSONPointer;
 
 class Test extends Operation implements Atomic
 {
@@ -17,7 +18,52 @@ class Test extends Operation implements Atomic
 
     public function apply(Pointer $target)
     {
+        try {
+            $targetValue = $target->get($this->path);
+        } catch (JSONPointer\Exception $e) {
+            throw new Exception($e->getMessage(), null, $e);
+        }
 
+        switch (gettype($this->value)) {
+            case 'array':
+                $this->assertArraysEquals($this->value, $targetValue);
+                break;
+
+            case 'string':
+                $this->assertStringsEquals($this->value, $targetValue);
+                break;
+
+            case 'double':
+            case 'integer':
+                $this->assertNumbersEquals($this->value, $targetValue);
+                break;
+
+            case 'bool':
+                $this->assertBoolEquals($this->value, $targetValue);
+                break;
+        }
+    }
+
+    public function assertStringsEquals($expected, $actual)
+    {
+        if (gettype($actual) !== 'string') {
+            throw new Exception('Target value is not a string');
+        }
+
+        if ($expected !== $actual) {
+            throw new Exception('Target value does not match expected value');
+        }
+    }
+
+    public function assertNumbersEquals($expected, $actual)
+    {
+        if (!in_array(gettype($actual), ['integer', 'double'])) {
+            throw new Exception('Target value is not a number');
+        }
+
+        if ($expected != $actual) {
+            throw new Exception('Target value does not match expected value');
+        }
     }
 
     public function revert(Pointer $target)
